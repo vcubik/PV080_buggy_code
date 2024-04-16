dc2071cd9d48097bb5a3ce0ac31bd902e1f80d2f
Viktor Čubík <xcubik@fi.muni.cz>
2024-04-16 15:12:40 +0200

    02

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,8 +26,11 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     """ Import the requested version (2 or 3) of urllib """
+    
     exec(f"import urllib{urllib_version} as urllib", globals())
+    
     # Fetch and print the requested URL
+
 
     try:
         http = urllib.PoolManager()
